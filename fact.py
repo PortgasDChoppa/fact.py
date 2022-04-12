5c9672d6ed2237b3567eab43bed805639d878745
Decko <dalsegnodecko@gmail.com>
2022-04-11 19:22:06 -0600

    Initial desing, pseudo-code

---
+++ fact.py
@@ -0,0 +1,14 @@
+#Simplified Sudoku
+#Creator: Diego Sebastian Cobos A.
+#11320699
+#Date created: April 10th, 2022
+#Instructor: Jeff Long
+#Description: Check if a simplified sudoku(file) is valid or not
+
+#Read the file
+#Create a nested list for the numbers in our sudoku
+#Take the sudoku size from the list
+#Create a list with all the numbers in the nested list
+#Calculate the number of times a number should be seen in the code
+#Count the number of times a number is present on the list
+#If the count is equal to one, the count is true, else, is false
