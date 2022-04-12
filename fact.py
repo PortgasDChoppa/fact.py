df19e8f749a8ada8fed4e516a857feee75e8230d
Decko <dalsegnodecko@gmail.com>
2022-04-11 19:27:41 -0600

    Added code to create a list with all the items of the nested list

--- fact.py
+++ fact.py
@@ -16,7 +16,14 @@ for line in f:
     line = [int(i) for i in line]
     sudoku.append(line)
 #Take the sudoku size from the list
+N = sudoku.pop(0)
+multiples = N[0]/3
 #Create a list with all the numbers in the nested list
+nums = []
+flag=0
+for i in range(0, len(sudoku)):
+    for j in range(0, len(sudoku)):
+        nums.append(sudoku[i][j])
 #Calculate the number of times a number should be seen in the code
 #Count the number of times a number is present on the list
 #If the count is equal to one, the count is true, else, is false
